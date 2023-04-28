df8ff2bc80a888d2da81d27ca478c74008884429
duguorong <dudurong009@gmail.com>
2023-04-28 13:19:57 +0800

    exercise: implement "anonymous functions"

--- exercises/anonymous_functions/anonymous_functions1/main.go
+++ exercises/anonymous_functions/anonymous_functions1/main.go
@@ -1,15 +1,14 @@
 // anonymous functions1
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
-
+	name := "Guorong"
 	func(name string) {
 		fmt.Printf("Hello %s", name)
-	}()
+	}(name)
 
 }
