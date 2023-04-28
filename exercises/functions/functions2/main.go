3e99a8e269ad8fd47ecc05a8a890d0a401d1beb2
duguorong <dudurong009@gmail.com>
2023-04-28 13:15:40 +0800

    exercise: implement "functions"

--- exercises/functions/functions2/main.go
+++ exercises/functions/functions2/main.go
@@ -1,7 +1,6 @@
 // functions2
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
@@ -10,7 +9,7 @@ func main() {
 	call_me(10)
 }
 
-func call_me(num) {
+func call_me(num int) {
 	for n := 0; n <= num; n++ {
 		fmt.Printf("Num is %d\n", n)
 	}
--- exercises/functions/functions4/main.go
+++ exercises/functions/functions4/main.go
@@ -1,7 +1,6 @@
 // functions4
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
@@ -10,6 +9,6 @@ func main() {
 	fmt.Println("1 + 2 is: ", add_numbers(1, 2)) // don't change this line
 }
 
-func add_numbers(a int, b int) {
+func add_numbers(a int, b int) int {
 	return a + b
 }
