c2813635e033551886141e24280057154ad9ef7a
duguorong <dudurong009@gmail.com>
2023-04-28 13:19:32 +0800

    exercise: implement "structs"

--- exercises/structs/structs1/main.go
+++ exercises/structs/structs1/main.go
@@ -1,15 +1,15 @@
 // structs1
 // Make me compile!
-//
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 type Person struct {
-
+	name string
+	age  uint
 }
 
 func main() {
+	person := Person{name: "John", age: 21}
 	fmt.Printf("Person %s and age %d", person.name, person.age)
 }
